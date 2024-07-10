1c6d6a5c9e86a5a4dc8b046fb9f6694302cb854f
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-07-09 22:20:19 -0500

    ❤️ Aplicacion base

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Semilleros Unipamplona",
+  description: "Aplicacion para el manejo de semilleros de la Unipamplona - Prototipo.",
 };
 
 export default function RootLayout({
@@ -17,11 +17,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
+    <html lang="es" className={GeistSans.className}>
       <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
+        {children}
       </body>
     </html>
   );
