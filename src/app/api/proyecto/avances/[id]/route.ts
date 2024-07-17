76752dbfa131b7e99d3231f9a50d9b3f22ad79bf
HAndres8 <andresfhernandez999@gmail.com>
2024-07-16 22:36:07 -0500

    Se agregan metodos POST, PUT y DELETE para avances de proyecto

--- src/app/api/proyecto/avances/[id]/route.ts
+++ src/app/api/proyecto/avances/[id]/route.ts
@@ -24,3 +24,68 @@ export async function GET(request: Request, context: Context) {
 
   return NextResponse.json([]);
 }
+
+/*
+POST /api/proyecto/avances/[idProyecto]
+
+Crear un nuevo avance en ese proyecto
+*/
+export async function POST(request: Request, context: Context) {
+  const { id } = context.params;
+  const params = await request.json();
+  const supabase = createClient();
+
+  const { data: avance } = await supabase.from("Avance").insert([
+    {
+      nombre: params.nombre,
+      fecha: params.fecha,
+      contenido: params.contenido,
+      id_proyecto: id
+    }])
+    .select();
+  
+  if (avance) {
+    return NextResponse.json(avance);
+  }
+  return NextResponse.json([]);
+}
+
+/*
+PUT /api/proyecto/avances/[idProyecto]
+
+Actualizar un avance de un proyecto
+*/
+export async function PUT(request: Request, context: Context) {
+  const params = await request.json();
+  const supabase = createClient();
+
+  const { data: avance } = await supabase.from("Avance").update({
+      nombre: params.nombre,
+      fecha: params.fecha,
+      contenido: params.contenido
+  })
+  .eq("id", params.id)
+  .select();
+
+  if (avance) {
+    return NextResponse.json(avance);
+  }
+  return NextResponse.json([]);
+}
+
+/*
+DELETE /api/proyecto/avances/[idProyecto]
+
+Eliminar un avance de un proyecto
+*/
+export async function DELETE(request: Request, context: Context) {
+  const params = await request.json();
+  const supabase = createClient();
+
+  const { data: avance } = await supabase.from("Avance").delete().eq("id", params.id).select();
+
+  if (avance) {
+    return NextResponse.json(avance);
+  }
+  return NextResponse.json([]);
+}
