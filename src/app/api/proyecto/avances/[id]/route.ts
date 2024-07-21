d1793e3346a1d35deb6d0f52dedf71342f7279ae
HAndres8 <andresfhernandez999@gmail.com>
2024-07-20 19:07:29 -0500

    Se agrega metodo eliminar avance. Correccion actualizar avance

--- src/app/api/proyecto/avances/[id]/route.ts
+++ src/app/api/proyecto/avances/[id]/route.ts
@@ -37,7 +37,6 @@ export async function POST(request: Request, context: Context) {
   const body = await request.json();
   const supabase = createClient();
 
-  const { data: proyecto } = await supabase.from("Proyecto").select("nombre").eq("id", id).single();
   const { data: avance } = await supabase.from("Avance").insert({
     nombre: body.nombre,
     fecha: body.fecha,
@@ -47,33 +46,35 @@ export async function POST(request: Request, context: Context) {
   .select()
   .single();
   
+  const { data: proyecto } = await supabase.from("Proyecto").select("nombre").eq("id", id).single();
   const misArchivos: File[] = body.archivos;
 
   if (misArchivos && avance) {
     await Promise.all(misArchivos.map(async (miArchivo) => {
       const { data } = await supabase.storage
         .from("archivos-avances")
-        .upload(`${proyecto?.nombre}/${miArchivo.name}`, miArchivo);
+        .upload(`${proyecto?.nombre}/${avance.id}/${miArchivo.name}`, miArchivo);
       
-    if(data){
-      const { data: archivo } = await supabase.from("Archivo").insert({
-        nombre: miArchivo.name,     // revisar si toca substring para quitar la extension del nombre
-        ruta: data.path,
-        id_avance: avance.id
-      });
-      
-      if(!archivo){
+      if(data){
+        const { data: archivo } = await supabase.from("Archivo").insert({
+          nombre: miArchivo.name,
+          ruta: data.path,
+          id_avance: avance.id
+        })
+        .select();
+        
+        if(!archivo){
+          return NextResponse.json(
+            {"mensaje": "No se pudo crear el archivo en el avance"},
+            {"status": 500}
+          );
+        }
+      }else{
         return NextResponse.json(
-          {"mensaje": "No se pudo crear el archivo en el avance"},
+          {"mensaje": "No se pudo almacenar el archivo"},
           {"status": 500}
         );
       }
-    }else{
-      return NextResponse.json(
-        {"mensaje": "No se pudo almacenar el archivo"},
-        {"status": 500}
-      );
-    }
     }));
   }
   
@@ -102,61 +103,43 @@ export async function PUT(request: Request, context: Context) {
     contenido: body.contenido
   })
   .eq("id", body.id)
-  .select("id, archivos: Archivo(id), rutas: Archivo(ruta)")
+  .select()
   .single();
 
   const { data: proyecto } = await supabase.from("Proyecto").select("nombre").eq("id", id).single();
   const misArchivos: File[] = body.archivos;
-  const archivosName = misArchivos.map(archivo => archivo.name);
 
-  if(misArchivos && avance){
-    // archivosE => guarda los archivos existentes que estan en el PUT
-    const archivosIds = avance.archivos.map(archivo => archivo.id);
-    const { data: archivosE } = await supabase.from("Archivo")
-      .select("id, nombre")
-      .in("id", archivosIds)
-      .in("nombre", archivosName);
-    const archivosEIds = archivosE?.map(archivo => archivo.id);
-    const archivosEName = archivosE?.map(archivo => archivo.nombre);
-
-    // Agregar nuevos archivos al storage
-    if(archivosEName){
-      await Promise.all(misArchivos.map(async (miArchivo) => {
-        if(!archivosEName.includes(miArchivo.name)){
-          const { data } = await supabase.storage
-            .from("archivos-avances")
-            .upload(`${proyecto?.nombre}/${miArchivo.name}`, miArchivo);
-          if(data){
-          // Agregar nuevos archivos a la tabla
-            await supabase.from("Archivo").insert({
-              nombre: miArchivo.name,
-              ruta: data.path,
-              id_avance: avance.id
-            });
-          }
+  if (misArchivos && avance) {
+    await Promise.all(misArchivos.map(async (miArchivo) => {
+      const { data } = await supabase.storage
+        .from("archivos-avances")
+        .upload(`${proyecto?.nombre}/${avance.id}/${miArchivo.name}`, miArchivo);
+      
+      if(data){
+        const { data: archivo } = await supabase.from("Archivo").insert({
+          nombre: miArchivo.name,
+          ruta: data.path,
+          id_avance: avance.id
+        })
+        .select();
+        
+        if(!archivo){
+          return NextResponse.json(
+            {"mensaje": "No se pudo crear el archivo en el avance"},
+            {"status": 500}
+          );
         }
-      }));
-    }
-
-    // Eliminar todos los archivos de ese avance que no tengan id en archivosRIds
-    const { data: eliminados } = await supabase.from("Archivo")
-      .delete()
-      .in("id", archivosIds)
-      .not("id", "in", archivosEIds)
-      .select();
-    
-    // Si encontro, elimina del storage esos archivos
-    if(eliminados){
-      await Promise.all(eliminados.map(async (eliminado) => {
-        await supabase.storage
-          .from("archivos-avances")
-          .remove([eliminado.ruta]);
-      }));
-    }
+      }else{
+        return NextResponse.json(
+          {"mensaje": "No se pudieron almacenar los archivos"},
+          {"status": 500}
+        );
+      }
+    }));
   }
 
   if (avance) {
-    return NextResponse.json(avance.id);
+    return NextResponse.json(avance);
   }
   return NextResponse.json(
     {"mensaje": "No se pudo actualizar el avance"},
