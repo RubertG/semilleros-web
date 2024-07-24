e41b21e17f50b6732fb3e5cb08b9e0ebf66d9da3
Leider17 <leidersolano133@gmail.com>
2024-07-24 00:18:03 -0500

    correccion permisos borrar avances e integrantes

--- src/components/projects/members/members-card.tsx
+++ src/components/projects/members/members-card.tsx
@@ -1,33 +1,52 @@
-"use client"
+"use client";
 
-import { FetchType } from "@/src/types/projects/projects"
-import { SquareMinus } from "../../common/icons"
-import Avvvatars from "avvvatars-react"
-import { defaultUrl } from "@/src/const/common/consts"
-import { useRouter } from "next/navigation"
+import { FetchType } from "@/src/types/projects/projects";
+import { SquareMinus } from "../../common/icons";
+import Avvvatars from "avvvatars-react";
+import { defaultUrl } from "@/src/const/common/consts";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Database } from "@/src/types/db/supabase";
+import { getRol } from "@/src/utils/supabase/get-rol-client";
 
-interface Props extends FetchType { }
+interface Props extends FetchType {}
 
-export const MembersCard = ({
-  estudiante, proyecto
-}: Props) => {
-  const router = useRouter()
+export const MembersCard = ({ estudiante, proyecto }: Props) => {
+  const router = useRouter();
+  const [rol, setRol] = useState<{
+    rol: Database["public"]["Enums"]["rol"] | null;
+    inProject: boolean;
+  }>({
+    rol: null,
+    inProject: false,
+  });
+
+  useEffect(() => {
+    const getR = async () => {
+      const rol = await getRol({ idProject: proyecto.id });
+      setRol(rol);
+    };
+    getR();
+  }, []);
 
   const handleDelete = async () => {
-    const res = await fetch(`${defaultUrl}/api/proyecto/integrantes/${proyecto.id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        id_estudiante: estudiante.id
-      })
-    })
+    const res = await fetch(
+      `${defaultUrl}/api/proyecto/integrantes/${proyecto.id}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id_estudiante: estudiante.id,
+        }),
+      }
+    );
 
     if (res.ok) {
-      router.refresh()
+      router.refresh();
     }
-  }
+  };
 
   return (
     <article className="flex items-center justify-between px-3.5 py-2.5 mb-1 lg:hover:bg-bg-200 rounded-lg lg:transition-colors">
@@ -38,12 +57,14 @@ export const MembersCard = ({
           <p className="text-sm text-text-300">{estudiante.correo}</p>
         </div>
       </div>
-      <button
-        className="lg:hover:scale-125 lg:transition-transform"
-        onClick={handleDelete}
-      >
-        <SquareMinus className="w-5 h-5 stroke-text-300" />
-      </button>
+      {rol.rol === "tutor" && (
+        <button
+          className="lg:hover:scale-125 lg:transition-transform"
+          onClick={handleDelete}
+        >
+          <SquareMinus className="w-5 h-5 stroke-text-300" />
+        </button>
+      )}
     </article>
-  )
-}
+  );
+};
--- src/components/projects/progress/progress-card.tsx
+++ src/components/projects/progress/progress-card.tsx
@@ -1,53 +1,71 @@
-"use client"
+"use client";
 
-import { ProgressType } from "@/src/types/projects/projects"
-import { SquareMinus } from "../../common/icons"
-import { defaultUrl } from "@/src/const/common/consts"
-import { useRouter } from "next/navigation"
+import { ProgressType } from "@/src/types/projects/projects";
+import { SquareMinus } from "../../common/icons";
+import { defaultUrl } from "@/src/const/common/consts";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Database } from "@/src/types/db/supabase";
+import { getRol } from "@/src/utils/supabase/get-rol-client";
 
 interface Props {
-  progress: ProgressType
+  progress: ProgressType;
 }
 
-export const ProgressCard = ({
-  progress
-}: Props) => {
-  const router = useRouter()
+export const ProgressCard = ({ progress }: Props) => {
+  const router = useRouter();
+  const [rol, setRol] = useState<{
+    rol: Database["public"]["Enums"]["rol"] | null;
+    inProject: boolean;
+  }>({
+    rol: null,
+    inProject: false,
+  });
+
+  useEffect(() => {
+    const getR = async () => {
+      const rol = await getRol({ idProject: progress.id_proyecto });
+      setRol(rol);
+    };
+    getR();
+  }, []);
 
   const handleDelete = async () => {
-    const res = await fetch(`${defaultUrl}/api/proyecto/avances/${progress.id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        id: progress.id
-      })
-    })
+    const res = await fetch(
+      `${defaultUrl}/api/proyecto/avances/${progress.id}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: progress.id,
+        }),
+      }
+    );
 
     if (res.ok) {
-      router.refresh()
+      router.refresh();
     }
-  }
+  };
 
   return (
     <article className="flex items-center justify-between px-3.5 py-2.5 mb-1 lg:hover:bg-bg-200 rounded-lg lg:transition-colors">
       <div className="text-ellipsis overflow-hidden whitespace-nowrap">
         <p className="text-text-100 font-medium">{progress.nombre}</p>
         <p className="text-sm text-text-300">
-          {new Date(progress.fecha).toLocaleDateString('es-ES', {
-            month: 'long',
-            year: 'numeric',
-            day: 'numeric'
+          {new Date(progress.fecha).toLocaleDateString("es-ES", {
+            month: "long",
+            year: "numeric",
+            day: "numeric",
           })}
         </p>
       </div>
-      <button
-        className="lg:hover:scale-125 lg:transition-transform"
-        onClick={handleDelete}
-      >
-        <SquareMinus className="w-5 h-5 stroke-text-300" />
-      </button>
+      {rol.rol === "tutor" && (
+        <button className="text-text-300" onClick={handleDelete}>
+          <SquareMinus />
+        </button>
+      )}
     </article>
-  )
-}
+  );
+};
